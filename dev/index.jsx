0bdf5a6120036a9a12dd4b11b2178304cb3974c3
miakdietrichvibe <mia.d@vibenomics.com>
2018-01-29 21:04:10 -0500

    Finished Module 3!

--- dev/index.jsx
+++ dev/index.jsx
@@ -3,3 +3,25 @@ import ReactDOM from "react-dom";
 
 require("./site.scss");
 
+
+const getUserInfo = () =>
+    ({
+        firstName: "Mia",
+        lastName: "Dietrich",
+    });
+
+function HelloWorld (props) {
+    return (
+        <div>Hello {props.user.firstName} {props.user.lastName}!</div>
+    )
+}
+
+var me = { firstName: "Mia", lastName: "Dietrich"}
+
+ReactDOM.render(
+    <HelloWorld user={getUserInfo()} />,
+    document.querySelector("#container")
+);
+
+
+
